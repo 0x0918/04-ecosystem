f3c7d3278a27eadb9462c437c93f6c2c3dd68853
0x0918 <yiyunbest@gmail.com>
2024-07-09 16:19:26 +0800

    feature: introduce tracing && tracing-subscriber

--- examples/err.rs
+++ examples/err.rs
@@ -1,7 +1,33 @@
 use std::{fs::File, mem::size_of, num::ParseIntError};
+use thiserror::Error;
 
 use anyhow::{Context, Error};
-use ecosystem::MyError;
+#[derive(Debug, Error)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Parse error: {0}")]
+    Parse(#[from] std::num::ParseIntError),
+
+    #[error("Serialize json error: {0}")]
+    Serialize(#[from] serde_json::Error),
+
+    #[error("Error: {0:?}")]
+    BigError(Box<BigError>),
+
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8; 64],
+    d: u64,
+}
 
 fn main() -> Result<(), Error> {
     println!("size of anyhow::Error is {}", size_of::<anyhow::Error>());
